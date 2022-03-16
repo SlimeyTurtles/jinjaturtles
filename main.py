c8082c591a3545bf84cf6fe7f15e32fd41b9fccc
nataliecohen <89223547+nataliecohen@users.noreply.github.com>
2022-03-16 14:29:02 -0700

    base, navbar, sassy

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+# import "packages" from flask
+from flask import Flask, render_template
+from flask import request
+import json
+import requests
+from __init__ import app
+import requests, random
 import Flask
+
