9444b7019172da945173831943cc4f4a5b03ccc1
paigemccartin <89223526+paigemccartin@users.noreply.github.com>
2022-04-07 11:49:07 -0700

    hello

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-# import "packages" from flask
-from flask import Flask, render_template
-from flask import request
-import json
-import requests
-from __init__ import app
-import requests, random
-import Flask
+from flask import render_template, Flask
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
