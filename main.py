3809bf2949eae12fa5098211dfeb7d29698b693f
avinh <avinhahuynh@gmail.com>
2022-05-12 02:42:14 -0700

    started all of yalls projects and cleaned code

--- main.py
+++ main.py
@@ -3,9 +3,27 @@ from flask import render_template, Flask
 app = Flask(__name__)
 
 
+# app routes
+
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/contact/')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/login/')
+def login():
+    return render_template("login/login.html")
+
+# recipes
+
+@app.route('/recipe-temp/')
+def recipetemp():
+    return render_template("recipes/recipe_base.html")
+
+# run code
 
 if __name__ == "__main__":
     app.run(debug=True)
