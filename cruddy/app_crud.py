35b26678d1a7b237153af147773513b0250f3ca9
avinh <avinhahuynh@gmail.com>
2022-06-01 03:34:28 -0700

    bro 24 files ://

--- cruddy/app_crud.py
+++ cruddy/app_crud.py
@@ -1,8 +1,7 @@
 """control dependencies to support CRUD app routes and APIs"""
 from flask import Blueprint, render_template, request, url_for, redirect, jsonify, make_response
 from flask_login import login_required
-
-from cruddy.query import *
+from cruddy.query import Users, users_all, user_by_id, users_ilike
 
 # blueprint defaults https://flask.palletsprojects.com/en/2.0.x/api/#blueprint-objects
 app_crud = Blueprint('crud', __name__,
@@ -26,65 +25,6 @@ def crud():
     return render_template("crud.html", table=users_all())
 
 
-# Flask-Login directs unauthorised users to this unauthorized_handler
-@login_manager.unauthorized_handler
-def unauthorized():
-    """Redirect unauthorized users to Login page."""
-    return redirect(url_for('crud.crud_login'))
-
-
-# if login url, show phones table only
-@app_crud.route('/login/', methods=["GET", "POST"])
-def crud_login():
-    # obtains form inputs and fulfills login requirements
-    if request.form:
-        email = request.form.get("email")
-        password = request.form.get("password")
-        if login(email, password):       # zero index [0] used as email is a tuple
-            return redirect(url_for('crud.crud'))
-
-    # if not logged in, show the login page
-    return render_template("login.html")
-
-
-@app_crud.route('/authorize/', methods=["GET", "POST"])
-def crud_authorize():
-    # check form inputs and creates user
-    if request.form:
-        # validation should be in HTML
-        user_name = request.form.get("user_name")
-        email = request.form.get("email")
-        password1 = request.form.get("password1")
-        password2 = request.form.get("password1")           # password should be verified
-        if authorize(user_name, email, password1):    # zero index [0] used as user_name and email are type tuple
-            return redirect(url_for('crud.crud_login'))
-    # show the auth user page if the above fails for some reason
-    return render_template("authorize.html")
-
-@app.route('templates/recipes/recipe.html', methods=["GET", "POST"])
-def recipes():
-    # obtains form inputs and fulfills login requirements
-    if request.form:
-        recipe = request.form.get("recipe")
-        recipeImage = request.form.get("recipeImage")
-        if login(email, password):       # zero index [0] used as email is a tuple
-            return redirect(url_for('crud.crud'))
-
-    # if not logged in, show the login page
-    return render_template("login.html")
-  
-@app.route('templates/feedback.html', methods=["GET", "POST"])
-def feedback():
-    # obtains form inputs and fulfills login requirements
-    if request.form:
-        recipe = request.form.get("recipe")
-        recipeImage = request.form.get("recipeImage")
-        if login(email, password):       # zero index [0] used as email is a tuple
-            return redirect(url_for('crud.crud'))
-
-    # if not logged in, show the login page
-    return render_template("login.html")
-
 # CRUD create/add
 @app_crud.route('/create/', methods=["POST"])
 def create():
@@ -101,15 +41,13 @@ def create():
 
 
 # CRUD read
-@app_crud.route('/read/', methods=["POST"])
-def read():
-    """gets userid from form and obtains corresponding data from Users table"""
+@app_crud.route('/read/<userID>')
+def read(userID):
+    """gets userid from form action"""
     table = []
-    if request.form:
-        userid = request.form.get("userid")
-        po = user_by_id(userid)
-        if po is not None:
-            table = [po.read()]  # placed in list for easier/consistent use within HTML
+    po = user_by_id(userID)
+    if po is not None:
+        table = [po.read()]  # placed in list for easier/consistent use within HTML
     return render_template("crud.html", table=table)
 
 
@@ -120,21 +58,22 @@ def update():
     if request.form:
         userid = request.form.get("userid")
         name = request.form.get("name")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        phone = request.form.get("phone")
         po = user_by_id(userid)
         if po is not None:
-            po.update(name)
+            po.update(name, email, password, phone)
     return redirect(url_for('crud.crud'))
 
 
 # CRUD delete
-@app_crud.route('/delete/', methods=["POST"])
-def delete():
-    """gets userid from form delete corresponding record from Users table"""
-    if request.form:
-        userid = request.form.get("userid")
-        po = user_by_id(userid)
-        if po is not None:
-            po.delete()
+@app_crud.route('/delete/<userID>')
+def delete(userID):
+    """gets userid from action"""
+    po = user_by_id(userID)
+    if po is not None:
+        po.delete()
     return redirect(url_for('crud.crud'))
 
 
@@ -151,5 +90,7 @@ def search_term():
     """ obtain term/search request """
     req = request.get_json()
     term = req['term']
-    response = make_response(jsonify(users_ilike(term)), 200)
+    json_ready = users_ilike(term)
+    json = jsonify(json_ready)
+    response = make_response(json, 200)
     return response
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+from flask import render_template, redirect, request, url_for, send_from_directory
+from flask_login import login_required
 
-from flask import Flask, render_template
-from flask import request
+from __init__ import app, login_manager
+from cruddy.app_crud import app_crud
+from uploady.app_upload import app_upload
+from cruddy.app_crud_api import app_crud_api
+from notey.app_notes import app_notes
 
+from cruddy.login import login, logout, authorize
 
+app.register_blueprint(app_upload)
+app.register_blueprint(app_crud)
+app.register_blueprint(app_crud_api)
+app.register_blueprint(app_notes)
 
-
-app = Flask(__name__)
 
 # app routes
 
@@ -24,10 +32,6 @@ def contact():
 @app.route('/schedule/')
 def schedule():
     return render_template("schedule.html")
-
-@app.route('/login/')
-def login():
-    return render_template("login/login.html")
 
 # recipes
 
@@ -65,6 +69,79 @@ def poll():
 @app.route('/gallery/')
 def gallery():
     return render_template("gallery.html")
+
+
+# cruddy crud
+
+# Flask-Login directs unauthorised users to this unauthorized_handler
+@login_manager.unauthorized_handler
+def unauthorized():
+    """Redirect unauthorized users to Login page."""
+    app.config['NEXT_PAGE'] = request.endpoint
+    return redirect(url_for('main_login'))
+
+
+# if login url, show phones table only
+@app.route('/login/', methods=["GET", "POST"])
+def main_login():
+    # obtains form inputs and fulfills login requirements
+    if request.form:
+        email = request.form.get("email")
+        password = request.form.get("password")
+        if login(email, password):
+            try:  # try to redirect to next page
+                next_page = app.config['NEXT_PAGE']
+                app.config['NEXT_PAGE'] = None
+                return redirect(url_for(next_page))
+            except:  # any failure goes to home page
+                return redirect(url_for('index'))
+
+    # if not logged in, show the login page
+    return render_template("login.html")
+
+
+# if login url, show phones table only
+@app.route('/logout/', methods=["GET", "POST"])
+@login_required
+def main_logout():
+    logout()
+    return redirect(url_for('index'))
+
+
+# user authorize with password validation
+@app.route('/authorize/', methods=["GET", "POST"])
+def main_authorize():
+    error_msg = ""
+    # check form inputs and creates user
+    if request.form:
+        # validation should be in HTML
+        user_name = request.form.get("user_name")
+        email = request.form.get("email")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")  # password should be verified
+        if password1 == password2:
+            if authorize(user_name, email, password1):
+                return redirect(url_for('main_login'))
+        else:
+            error_msg = "Passwords do not match"
+    # show the auth user page if the above fails for some reason
+    return render_template("authorize.html", error_msg=error_msg)
+
+
+# serve uploaded files so they can be downloaded by users.
+@app.route('/uploads/<name>')
+def uploads_endpoint(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+# register "uploads_endpoint" endpoint so url_for will find all uploaded files
+app.add_url_rule(
+    "/" + app.config['UPLOAD_FOLDER'] + "/<name>", endpoint="uploads_endpoint", build_only=True
+)
+
+# 404 error page redirect
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == "__main__":
